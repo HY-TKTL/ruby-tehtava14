9105fa0cfc6b87e9a29c092174f9b8561a511498
Lensuman <jevgeni.lensu@gmail.com>
2016-01-20 04:11:48 +0200

    Update testi_spec.rb

--- testi_spec.rb
+++ testi_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe 'luokka Pelaaja' do
-  it "kopioitu edellisestä tehtävästä" do
+  it "kopioitu edellisesta tehtavasta" do
     expect(File.exists? './pelaaja.rb' ).to be true
     require './pelaaja.rb'
     Pelaaja
@@ -7,7 +7,7 @@ RSpec.describe 'luokka Pelaaja' do
 end
 
 RSpec.describe 'luokka Joukkue' do
-  it "on määritelty oikeassa tiedostossa" do
+  it "on maaritelty oikeassa tiedostossa" do
     expect(File.exists? './joukkue.rb' ).to be true
     require './joukkue.rb'
     Joukkue
@@ -16,23 +16,23 @@ RSpec.describe 'luokka Joukkue' do
   it "konstruktori ja to_s toimivat" do
     require './joukkue.rb'
     j1 = Joukkue.new "Kumpulan pallo"
-    expect(j1.to_s).to eq('Kumpulan pallo, pelaajia 0, maaleja yhteensä 0')
+    expect(j1.to_s).to eq('Kumpulan pallo, pelaajia 0, maaleja yhteensa 0')
 
     j2 = Joukkue.new "Alppilan urheilijat"
-    expect(j2.to_s).to eq('Alppilan urheilijat, pelaajia 0, maaleja yhteensä 0')
+    expect(j2.to_s).to eq('Alppilan urheilijat, pelaajia 0, maaleja yhteensa 0')
   end
 
-  it "pelaajien lisääminen" do
+  it "pelaajien lisaaminen" do
     require './joukkue.rb'
     j = Joukkue.new "Kumpulan pallo"
     j.lisaa_pelaaja Pelaaja.new('chang', 170)
-    expect(j.to_s).to eq('Kumpulan pallo, pelaajia 1, maaleja yhteensä 0')
+    expect(j.to_s).to eq('Kumpulan pallo, pelaajia 1, maaleja yhteensa 0')
     j.lisaa_pelaaja Pelaaja.new('arto', 181)
     j.lisaa_pelaaja Pelaaja.new('ada', 160)
-    expect(j.to_s).to eq('Kumpulan pallo, pelaajia 3, maaleja yhteensä 0')
+    expect(j.to_s).to eq('Kumpulan pallo, pelaajia 3, maaleja yhteensa 0')
   end
 
-  it "maalien määrä" do
+  it "maalien maara" do
     require './joukkue.rb'
     j = Joukkue.new "Kumpulan pallo"
     p1 = Pelaaja.new('chang', 170)
@@ -42,17 +42,17 @@ RSpec.describe 'luokka Joukkue' do
     j.lisaa_pelaaja p2
     j.lisaa_pelaaja p3
     expect(j.maaleja_yhteensa).to eq 0
-    expect(j.to_s).to eq('Kumpulan pallo, pelaajia 3, maaleja yhteensä 0')
+    expect(j.to_s).to eq('Kumpulan pallo, pelaajia 3, maaleja yhteensa 0')
     p1.lisaa_maali
     expect(j.maaleja_yhteensa).to eq 1
-    expect(j.to_s).to eq('Kumpulan pallo, pelaajia 3, maaleja yhteensä 1')
+    expect(j.to_s).to eq('Kumpulan pallo, pelaajia 3, maaleja yhteensa 1')
     3.times{ p2.lisaa_maali }
     7.times{ p3.lisaa_maali }
     expect(j.maaleja_yhteensa).to eq 11
-    expect(j.to_s).to eq('Kumpulan pallo, pelaajia 3, maaleja yhteensä 11')
+    expect(j.to_s).to eq('Kumpulan pallo, pelaajia 3, maaleja yhteensa 11')
   end
 
-  it "paras maalintekijä" do
+  it "paras maalintekija" do
     require './joukkue.rb'
     j = Joukkue.new "Kumpulan pallo"
     p1 = Pelaaja.new('chang', 170)
