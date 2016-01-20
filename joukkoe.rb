caeb645d6f1b388cca0cf345b532031896cfa602
Lensuman <jevgeni.lensu@gmail.com>
2016-01-20 04:01:34 +0200

    Update joukkoe.rb

--- joukkoe.rb
+++ joukkoe.rb
@@ -1,1 +1,32 @@
 
+class Joukkue
+ def initialize(nimi)
+  @nimi = nimi
+  @pelaajat = []
+ end
+
+ def lisaa_pelaaja(p)
+  @pelaajat << p
+ end
+
+ def maaleja_yhteensa
+  sum = 0
+  @pelaajat.length.times do |i|
+   sum = sum + @pelaajat.at(i).maaleja
+  end
+  sum
+ end
+ 
+ def paras_maalintekija
+  p = @pelaajat.at(0)
+  m = 0
+  @pelaajat.length.times do |i|
+   p = @pelaajat.at(i) and m = @pelaajat.at(i).maaleja if @pelaajat.at(i).maaleja > m
+  end
+  p	
+ end
+
+ def to_s
+  "#{@nimi}, pelaajia #{@pelaajat.length}, maaleja yhteensä #{self.maaleja_yhteensa}"
+ end
+end
